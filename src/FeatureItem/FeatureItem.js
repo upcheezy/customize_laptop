74c61dd7dbe7ea0cd73745bfe96ba4084478ff09
Matthew Upchurch <upcheezy@gmail.com>
2020-03-18 09:37:34 -0400

    extracted items component from feature items

--- src/FeatureItem/FeatureItem.js
+++ src/FeatureItem/FeatureItem.js
@@ -1,41 +1,16 @@
 import React from 'react';
-import slugify from 'slugify';
+import Item from '../Item/Item'
 
 export default function FeatureItem(props) {
-    const features = Object.keys(props.features).map((feature, idx) => {
-        const featureHash = feature + '-' + idx;
-        const options = props.features[feature].map(item => {
-            const itemHash = slugify(JSON.stringify(item));
-            return (
-                <div key={itemHash} className="feature__item">
-                    {/* cusomize item component  */}
-                    <input
-                        type="radio"
-                        id={itemHash}
-                        className="feature__option"
-                        name={slugify(feature)}
-                        checked={item.name === props.selection[feature].name}
-                        onChange={e => props.handleUpdate(feature, item)}
-                    />
-                    <label htmlFor={itemHash} className="feature__label">
-                        {item.name} ({props.currencyFormat.format(item.cost)})
-        </label>
-                </div>
-            );
-        });
-
-        return (
-            <fieldset className="feature" key={featureHash}>
-                <legend className="feature__name">
-                    <h3>{feature}</h3>
-                </legend>
-                {options}
-            </fieldset>
-        );
-    });
     return (
         <>
-            {features}
+            <Item
+                handleUpdate={props.handleUpdate}
+                features={props.features}
+                selection={props.selection}
+                currencyFormat={props.currencyFormat}
+            />
+            {/* {features} */}
         </>
     )
 
